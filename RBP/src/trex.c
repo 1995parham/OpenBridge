f1aa907c7bc142f0fab0f5ff1898ae3e15c47e2c
Parham Alvani <parham.alvani@gmail.com>
2016-01-12 12:24:00 +0330

    Header files were corrected.

--- RBP/src/trex.c
+++ RBP/src/trex.c
@@ -1,38 +1,15 @@
 /*
- * ################################################################################
- * #
- * #  Project F-Bridge                 [project home-page]
- * #
- * #  File:
- * #        /RBP/src/trex.c
- * #
- * #  Copyright notice:
- * #        Copyright (C) 2014, 2015 Project F-Bridge Group, CEIT Department,
- * #                       Amirkabir University of Technology (Tehran Polytechnic)
- * #
- * #  Author(s):
- * #	Parham Alvani (parham.alvani@gmail.com)
- * #
- * #  Description:
- * #	T-REX module implementation. see trex.h for more information.
- * #
- * #  Licence:
- * #        This file is part of the F-Bridge Project package.
- * #
- * #        This file is free code: you can redistribute it and/or modify it under
- * #        the terms of the GNU Lesser General Public License version 2.1 as
- * #        published by the Free Software Foundation.
- * #
- * #        This package is distributed in the hope that it will be useful, but
- * #        WITHOUT ANY WARRANTY; without even the implied warranty of
- * #        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * #        Lesser General Public License for more details.
- * #
- * #        You should have received a copy of the GNU Lesser General Public
- * #        License along with the F-Bridge source package.  If not, see
- * #        http://www.gnu.org/licenses/.
- * #
- * #
+ * In The Name Of God
+ * ========================================
+ * [] File Name : trex.c
+ *
+ * [] Creation Date : 12-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
 */
 #include <glib.h>
 #include <stdlib.h>
--- RBP/src/trex.h
+++ RBP/src/trex.h
@@ -1,42 +1,18 @@
 /*
- * ################################################################################
- * #
- * #  Project F-Bridge                 [project home-page]
- * #
- * #  File:
- * #        /RBP/src/trex.h
- * #
- * #  Copyright notice:
- * #        Copyright (C) 2014, 2015 Project F-Bridge Group, CEIT Department,
- * #                       Amirkabir University of Technology (Tehran Polytechnic)
- * #
- * #  Author(s):
- * #	Parham Alvani (parham.alvani@gmail.com)
- * #
- * #  Description:
- * #	T-REX module header file includes it's public interface.
- * #    this module work as REST proxy server at the end of RBP pipeline.
- * #
- * #  Licence:
- * #        This file is part of the F-Bridge Project package.
- * #
- * #        This file is free code: you can redistribute it and/or modify it under
- * #        the terms of the GNU Lesser General Public License version 2.1 as
- * #        published by the Free Software Foundation.
- * #
- * #        This package is distributed in the hope that it will be useful, but
- * #        WITHOUT ANY WARRANTY; without even the implied warranty of
- * #        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * #        Lesser General Public License for more details.
- * #
- * #        You should have received a copy of the GNU Lesser General Public
- * #        License along with the F-Bridge source package.  If not, see
- * #        http://www.gnu.org/licenses/.
- * #
- * #
+ * In The Name Of God
+ * ========================================
+ * [] File Name : trex.h
+ *
+ * [] Creation Date : 12-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
 */
-#ifndef PROXY_H
-#define PROXY_H
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#ifndef TREX_H
+#define TREX_H
 
 void trex_callback(SoupServer *server, SoupMessage *msg,
 	const char *path, GHashTable *query, const char *dest);
