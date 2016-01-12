ae395d6240614537ac9ac2bcca55afdf311e03f7
Parham Alvani <parham.alvani@gmail.com>
2016-01-12 12:24:00 +0330

    Header files were corrected.

--- RBP/src/rest.h
+++ RBP/src/rest.h
@@ -1,40 +1,15 @@
 /*
- * ################################################################################
- * #
- * #  Project F-Bridge                 [project home-page]
- * #
- * #  File:
- * #        /RBP/src/rest.h
- * #
- * #  Copyright notice:
- * #        Copyright (C) 2014, 2015 Project F-Bridge Group, CEIT Department,
- * #                       Amirkabir University of Technology (Tehran Polytechnic)
- * #
- * #  Author(s):
- * #	Parham Alvani (parham.alvani@gmail.com)
- * #
- * #  Description:
- * #	REST parser module header file includes it's public interface.
- * #    if you want to change REST parser module you must implements
- * #    these functions.
- * #
- * #  Licence:
- * #        This file is part of the F-Bridge Project package.
- * #
- * #        This file is free code: you can redistribute it and/or modify it under
- * #        the terms of the GNU Lesser General Public License version 2.1 as
- * #        published by the Free Software Foundation.
- * #
- * #        This package is distributed in the hope that it will be useful, but
- * #        WITHOUT ANY WARRANTY; without even the implied warranty of
- * #        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * #        Lesser General Public License for more details.
- * #
- * #        You should have received a copy of the GNU Lesser General Public
- * #        License along with the F-Bridge source package.  If not, see
- * #        http://www.gnu.org/licenses/.
- * #
- * #
+ * In The Name Of God
+ * ========================================
+ * [] File Name : rest.h
+ *
+ * [] Creation Date : 12-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
 */
 #ifndef REST_H
 #define REST_H
